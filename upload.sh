bac2ae2c01de3cb764899feb1019088af1f81d3a
Spencer Kiser <spencer.kiser@metmuseum.org>
2021-01-21 07:57:35 -0500

    Open Access csv

---
+++ upload.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#eval "$(ssh-agent -s)" || true > /dev/null
+#ssh-add /c/mma/ssh/id_rsa > /dev/null
+#git lfs track "*.csv"
+#cd /c/repos/openaccess
+#git pull
+#git add *
+#git commit -m 'Open Access csv'
+#git push
